9436c4af0e2473b3ed4c5d755aad6ab095bee2b8
Aadil <aadilhabibsattar@gmail.com>
2025-07-16 15:49:15 +0600

    styled and displayed api response information on page

--- src/index.js
+++ src/index.js
@@ -3,29 +3,6 @@ import { svgs } from "./svg.js";
 const APIKEY = "3HXW7VSTW8PT22AYJVU6ZKY35";
 let city = "dhaka";
 
-// async function getWeatherData() {
-//     return await fetch(
-//         `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=${APIKEY}&contentType=json`,
-//         { mode: "cors" }
-//     );
-// }
-
-// async function logWeatherData() {
-//     try {
-//         const response = await getWeatherData();
-//         if (!response.ok) {
-//             throw new Error("Resposne not OK");
-//         }
-
-//         const data = await response.json();
-//         console.log(data);
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
-
-// logWeatherData();
-
 const locationDropdownToggle = document.querySelector(
     ".location-dropdown-toggle"
 );
@@ -35,7 +12,62 @@ locationDropdownToggle.addEventListener("click", () => {
     locationDropdown.classList.toggle("hidden");
 });
 
-// const currentWeatherIcon = document.querySelector(".current-weather-icon");
-// const cloudyDayIcon = svgs["partlyCloudyDay"];
+async function fetchWeatherData() {
+    return await fetch(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=${APIKEY}&contentType=json`,
+        { mode: "cors" }
+    );
+}
 
-// currentWeatherIcon.innerHTML += cloudyDayIcon;
+async function getWeatherData() {
+    try {
+        const response = await fetchWeatherData();
+        if (!response.ok) {
+            throw new Error("Resposne not OK");
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const weatherData = await getWeatherData();
+console.log(weatherData);
+
+const address = document.querySelector(".address");
+const currentWeatherIcon = document.querySelector(".current-weather-icon-div");
+const weatherIcon = svgs[weatherData.currentConditions.icon];
+const currentTemp = document.querySelector(".current-temp");
+const currentConditions = document.querySelector(".current-conditions");
+const minTemp = document.querySelector(".min-temp-value");
+const maxTemp = document.querySelector(".max-temp-value");
+const feelsLike = document.querySelector(".feels-like-value");
+
+currentWeatherIcon.innerHTML += weatherIcon;
+address.innerHTML += weatherData.resolvedAddress;
+currentTemp.innerHTML += `${weatherData.currentConditions.temp}&deg;`;
+currentConditions.innerHTML += `${weatherData.currentConditions.conditions}`;
+
+minTemp.innerHTML += `${weatherData.days[0].tempmin}&deg`;
+maxTemp.innerHTML += `${weatherData.days[0].tempmax}&deg`;
+feelsLike.innerHTML += `${weatherData.currentConditions.feelslike}&deg;`;
+
+const sunriseValue = document.querySelector(".sunrise-value");
+const rainValue = document.querySelector(".rain-value");
+const humidityValue = document.querySelector(".humidity-value");
+const windSpeedValue = document.querySelector(".wind-speed-value");
+const pressureValue = document.querySelector(".pressure-value");
+const sunsetValue = document.querySelector(".sunset-value");
+const windDirectionValue = document.querySelector(".wind-direction-value");
+const uvValue = document.querySelector(".uv-value");
+
+sunriseValue.innerHTML += weatherData.currentConditions.sunrise;
+sunsetValue.innerHTML += weatherData.currentConditions.sunset;
+rainValue.innerHTML += `${weatherData.currentConditions.precipprob}%`;
+humidityValue.innerHTML += `${weatherData.currentConditions.humidity}%`;
+pressureValue.innerHTML += `${weatherData.currentConditions.pressure} mb`;
+windSpeedValue.innerHTML += `${weatherData.currentConditions.windspeed} mph`;
+windDirectionValue.innerHTML += `${weatherData.currentConditions.winddir}&deg;`;
+uvValue.innerHTML += `${weatherData.currentConditions.uvindex}`;
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/index.html",
-            favicon: "./src/assets/sun.svg",
+            favicon: "./src/assets/favicon.svg",
             cache: false,
         }),
     ],
